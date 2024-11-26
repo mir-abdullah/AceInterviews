7e284a95b7395d3eae4f68813009e6f185caedf4
mir-abdullah <mirabdullah04@gmail.com>
2024-11-27 01:29:46 +0500

    Revert "Merge branch 'farrukh' of https://github.com/mir-abdullah/AceInterviews into mir"
    This reverts commit 5fda0a0ef129432e91048d83b69edfd28869ebbc, reversing
    changes made to b7b3e9a2bbbf801c84179239b0c9b6fbe957d2d4.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Provider } from "react-redux";
+import { Provider } from "react-redux"; 
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import store from "./redux/store/store.js";
+import { ToastContainer } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css'; 
+import store from './redux/store/store.js';
 import App from "./App.jsx";
 import "./index.css";
-import Landingpage from "./pages/landingpage/Landingpage.jsx";
+import Landingpage from './pages/landingpage/Landingpage.jsx';
 import SignupPage from "./pages/signup/SignupPage.jsx";
 import LoginPage from "./pages/login/LoginPage.jsx";
 import RootLayout from "./RootLayout.jsx";
@@ -17,25 +17,24 @@ import Behavioral from "./pages/interviews/Behavioral.jsx";
 import Technical from "./pages/interviews/Technical.jsx";
 import TechInterviewPage from "./pages/interviews/TechInterviewPage.jsx";
 import Feedback from "./pages/feedback/Feedback.jsx";
-import Quizes from "./pages/quiz/Quizes.jsx";
+import Quizes from './pages/quiz/Quizes.jsx';
 import QuizPage from "./pages/quiz/QuizPage.jsx";
 import Profile from "./pages/profile/Profile.jsx";
 import InstructionsPage from "./pages/interviews/InstructionsPage.jsx";
 import RootLayoutAdmin from "./RootLayoutAdmin.jsx";
-import OverviewAdmin from "./pages/admin/OverviewAdmin.jsx";
-import BehavioralAdmin from "./pages/admin/BehaviouralAdmin.jsx";
-import TechnicalAdmin from "./pages/admin/TechnicalAdmin.jsx";
+import OverviewAdmin from "./pages/admin/OverviewAdmin.jsx"
+import BehavioralAdmin from "./pages/admin/BehaviouralAdmin.jsx"
+import TechnicalAdmin from "./pages/admin/TechnicalAdmin.jsx"
 import AddTechScenario from "./pages/admin/AddTechScenario.jsx";
 import ReviewFeedback from "./pages/admin/ReviewFeedback.jsx";
 import ProfileAdmin from "./pages/admin/ProfileAdmin.jsx";
 import AdminLogin from "./pages/admin/AdminLogin.jsx";
 import QuestionsPage from "./pages/QuestionsPage/QuestionsPage.jsx";
-import TechnicalQuestions from "./pages/QuestionsPage/TechnicalQuestions.jsx";
+import TechnicalQuestions from './pages/QuestionsPage/TechnicalQuestions.jsx'
 import QuizAdmin from "./pages/admin/QuizAdmin.jsx";
 import QuizQuestions from "./pages/QuestionsPage/QuizQuestions.jsx";
 import Results from "./pages/results/ResultMain.jsx";
 import BehaviouralInterview from "./pages/interviews/BehaviouralInterview.jsx";
-import LanguageMain from "./pages/languagetest/LanguageMain.jsx";
 
 const router = createBrowserRouter([
   {
@@ -51,8 +50,9 @@ const router = createBrowserRouter([
     element: <LoginPage />,
   },
   {
-    path: "/admin/login",
-    element: <AdminLogin />,
+    path:"/admin/login",
+    element: <AdminLogin/>
+
   },
   {
     path: "/admin",
@@ -64,15 +64,15 @@ const router = createBrowserRouter([
       { path: "techscenario", element: <AddTechScenario /> },
       { path: "reviewfeedback", element: <ReviewFeedback /> },
       { path: "profileadmin", element: <ProfileAdmin /> },
-      { path: "behaviour/questions", element: <QuestionsPage /> },
-      { path: "technical/questions", element: <TechnicalQuestions /> },
-      { path: "quizes", element: <QuizAdmin /> },
-      { path: "quiz/questions", element: <QuizQuestions /> },
+      {path:'behaviour/questions',element:<QuestionsPage/>},
+      {path:"technical/questions",element:<TechnicalQuestions/>},
+      {path:'quizes',element:<QuizAdmin/>},
+      {path:'quiz/questions',element:<QuizQuestions />}
       // { path: "quizzes", element: <Quizzes /> },
       // { path: "feedback", element: <FeedbackAdmin /> },
     ],
   },
-
+ 
   {
     path: "dashboard",
     element: <RootLayout />,
@@ -86,23 +86,23 @@ const router = createBrowserRouter([
         element: <Behavioral />,
       },
       {
-        path: "behavioral-interviews",
-        element: <BehaviouralInterview />,
+        path:"behavioral-interviews",
+        element:<BehaviouralInterview/>
       },
       {
         path: "technical",
         element: <Technical />,
       },
       {
-        path: "instructions/:interviewId",
-        element: <InstructionsPage />,
+        path:"instructions/:interviewId",
+        element:<InstructionsPage/>
       },
       {
         path: "techinterviewpage/:interviewId",
         element: <TechInterviewPage />,
       },
       {
-        path: "result-details/:type/:id",
+        path:"result-details/:type/:id",
         element: <Feedback />,
       },
       {
@@ -110,22 +110,18 @@ const router = createBrowserRouter([
         element: <Quizes />,
       },
       {
-        path: "results",
-        element: <Results />,
+        path: 'results' ,element:<Results/>
       },
-      {
-        path: "language-proficiency",
-        element: <LanguageMain />,
-      },
-
+      
       {
         path: "/dashboard/quiz/:quizTopicId",
         element: <QuizPage />,
       },
       {
         path: "/dashboard/profile",
-        element: <Profile />,
+        element: <Profile />, 
       },
+         
     ],
   },
 ]);
@@ -133,9 +129,12 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="266863559400-es7p8gjuvrde2osfh4rho8bc4re5v4rb.apps.googleusercontent.com">
-      <Provider store={store}>
+      <Provider store={store}> 
         <RouterProvider router={router} />
-        <ToastContainer theme="colored" autoClose={3000} />
+        <ToastContainer
+        theme="colored"
+        autoClose={3000} 
+        /> 
       </Provider>
     </GoogleOAuthProvider>
   </StrictMode>
--- frontend/src/pages/languagetest/LanguageMain.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const LanguageMain = () => {
-  return <div>LanguageMain</div>;
-};
-export default LanguageMain;
